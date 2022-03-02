8dd726847ef2b383cc44865a7aa1d21e650e4034
Rahul Bhardwaj <62097178+Rahulbhardwaj2000@users.noreply.github.com>
2022-03-02 16:31:40 +0530

    Added solution - LeetHub

---
+++ Sum Tree - GFG/sum-tree.java
@@ -0,0 +1,143 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+			
+			    if(g.isSumTree(root) == true)
+    			    System.out.println(1);
+    			else 
+    			    System.out.println(0);
+                t--;
+            
+        }
+    }
+  
+}
+
+// } Driver Code Ends
+
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+class Solution
+{
+	boolean isSumTree(Node root)
+	{
+	    return solve(root)[1]==1?true:false;
+	}
+	
+	int[] solve(Node root){
+	    int[] narr = new int[2];// sum of subtree, isTheTreeASumTree
+	    narr[1] = 1;
+	    // 0 means false and 1 means true
+	    if(root == null){
+	        return narr;
+	    }
+	    if(root.left == null && root.right == null){
+	        narr[0] = root.data;
+	        return narr;
+	    }
+	    int[] la = solve(root.left);
+	    int[] ra = solve(root.right);
+	    narr[0] = la[0] + ra[0] + root.data;
+	    if(la[1]==0 || ra[1]==0 || la[0]+ra[0] != root.data){
+	        narr[1] = 0;
+	        return narr;
+	    }
+	    return narr;
+	}
+}
