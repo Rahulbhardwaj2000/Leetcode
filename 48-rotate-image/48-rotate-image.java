f02520c83701dd83502f22f2295642ea68642eb1
Rahul Bhardwaj <62097178+Rahulbhardwaj2000@users.noreply.github.com>
2022-03-04 12:17:28 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (41.63%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,6 +1,7 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        // rotate = transpose(about top left to bottom right diagonal) + reflect about middle column
+        /*
+        // rotate = transpose (about top left to bottom right diagonal) + reflect (about middle column)
         int n = matrix.length;
         // tranpose
         for(int slide = 0;slide<n-1;slide++){
@@ -13,12 +14,27 @@ class Solution {
         
         // reflect
         for(int i=0;i<n;i++){
-            for(int j=0;j<n/2;j++){
+            for(int j=0;j<n/2;j++){ //important to keep it n/2 only here, even in case of EVEN length
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[i][n-1-j];
                 matrix[i][n-1-j] = temp;
             }
         }
-        
+        */
+        method2(matrix);
     }
+    
+    public void method2(int[][] matrix){ // needs to be performed on thsi particular order only
+        int n = matrix.length;
+        for(int i=0;i<(n/2 + n%2);i++){
+            for(int j=0;j<n/2;j++){
+                int temp = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+    
 }
