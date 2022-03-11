bd208596668fc5a41cca9d800052497aee82d496
Rahul Bhardwaj <62097178+Rahulbhardwaj2000@users.noreply.github.com>
2022-03-11 14:06:10 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (28.43%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,46 +1,17 @@
 class Solution {
-    static int mod = 1000000007;
+    // (1x2x3x4x....xn) x (1x3x5x7x....x2n-1)
+    // pickups              deliveries
     public int countOrders(int n) {
-        int[][] pd = new int[2][n];
-        // no. of pickups left, and no. of deliveries left
+        int mod = 1000000007;
+        long ans = 1;
+        for(int i=1;i<=n;i++){
+            ans *= i;
+            ans %= mod;
+            ans *= (2*i-1);
+            ans %= mod;
+        }
         
-        long[][] strg = new long[n+1][n+1];
-        solve(pd, 0, n, n, 0, strg); 
-        
-        return (int)strg[n][0]%mod;
+        return (int)ans%mod;
     }
     
-    public long solve(int[][] pd, int completed, int n, int nopl, int nodl, long[][] strg){
-        if(completed == n){
-            strg[nopl][nodl] = (strg[nodl][nopl] + 1)%mod;
-            return strg[nopl][nodl];
-        }
-        
-        if(strg[nopl][nodl]>0){
-            return strg[nopl][nodl];
-        }
-        
-        long ct = 0;
-        // picking up a delivery
-        for(int i=0;i<n;i++){
-            if(pd[0][i]==0){
-                pd[0][i] = 1;
-                ct += solve(pd, completed, n, nopl-1, nodl+1, strg);
-                pd[0][i] = 0;
-            }
-        }
-        
-        // delivering
-        for(int i=0;i<n;i++){
-            if(pd[0][i]==1 && pd[1][i]==0){
-                pd[1][i] = 1;
-                ct += solve(pd, completed+1, n, nopl, nodl-1, strg);
-                pd[1][i] = 0;
-            }
-        }
-        
-        ct %= mod;
-        strg[nopl][nodl] = ct;
-        return ct;
-    }
 }
