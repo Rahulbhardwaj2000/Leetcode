3abb6341345a5a799729d010d14cc48137cb2775
Rahul Bhardwaj <62097178+Rahulbhardwaj2000@users.noreply.github.com>
2022-03-23 00:13:08 +0530

    Time: 21 ms (82.01%), Space: 63.3 MB (56.61%) - LeetHub

---
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String getSmallestString(int n, int k) {
+        
+        
+        return solve(n, k);
+    }
+    
+    public String solve(int n, int k){
+        char[] answer = new char[n];
+        Arrays.fill(answer, 'a');
+        k = k - n;
+        while (n > 0 && k > 0) {
+            int min = Math.min(25, k);
+            answer[--n] = (char) (min + 'a');
+            k -= min;
+        }
+        return String.valueOf(answer);
+    }
+}
