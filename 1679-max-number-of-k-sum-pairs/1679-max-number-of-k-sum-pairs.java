75096bf0186af05417f682f42b7e56242ba7ee5d
Rahul Bhardwaj <62097178+Rahulbhardwaj2000@users.noreply.github.com>
2022-05-04 10:02:22 +0530

    Time: 62 ms (27.29%), Space: 84.2 MB (26.53%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -5,14 +5,15 @@ class Solution {
         
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(k-nums[i])){
-                System.out.println(k-nums[i] + "  idx: " + i);
                 count += 1;
                 int freq = map.get(k-nums[i]);
                 
                 if(freq == 1) map.remove(k-nums[i]);
                 else map.put(k-nums[i], freq-1);
                 
-            }else map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            }else{
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            }
         }
         
         return count;
