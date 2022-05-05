15a37d1979c25346a14be26ebf43d25058694fcc
Rahul Bhardwaj <62097178+Rahulbhardwaj2000@users.noreply.github.com>
2022-05-05 09:56:00 +0530

    Time: 42 ms (85.90%), Space: 43.6 MB (90.65%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,24 +1,41 @@
 class Solution {
-    public int minCostConnectPoints(int[][] points) {
-        
+    public int minCostConnectPoints(int[][] points) {    
         int cost = 0;
-        int[] start = points[0];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[2]-b[2]);
         boolean[] vis = new boolean[points.length];
-        pq.add(new int[]{start[0], start[1], 0, 0}); // x,y,cost, which point?
         
-        while(!pq.isEmpty()){
-            int[] rn = pq.poll();
-            if(vis[rn[3]]) continue;
-            vis[rn[3]] = true;
-            cost += rn[2];
+        // arr to track min edge wt to reach node i from any node already visited
+        int[] minDist = new int[points.length]; 
+        Arrays.fill(minDist, Integer.MAX_VALUE);
+        minDist[0] = 0;
+        
+        int countEdgesVisited = 0;
+        
+        while(countEdgesVisited < points.length){
+            int minWtEdge = Integer.MAX_VALUE;
+            int node = -1;
             
+            // getting the least weigted edge that is unvisited
             for(int i=0;i<points.length;i++){
-                if(vis[i]) continue;
-                int[] pt = points[i];
-                int dist = Math.abs(pt[0]-rn[0]) + Math.abs(pt[1]-rn[1]);
-                pq.add(new int[]{pt[0], pt[1], dist, i});
+                if(!vis[i] && minDist[i]<minWtEdge){
+                    minWtEdge = minDist[i];
+                    node = i;
+                }
             }
+            
+            vis[node] = true;
+            countEdgesVisited++;
+            cost += minDist[node];
+            
+            // updating the distance from node to all its neighbouring nodes
+            for(int i=0;i<points.length;i++){
+                if(!vis[i]){
+                    int[] pt = points[i];
+                    int wt = Math.abs(pt[0]-points[node][0]) + Math.abs(pt[1]-points[node][1]);
+                    if(wt < minDist[i]) minDist[i] = wt;
+                }
+            }
+            
+            
         }
         
         return cost;
