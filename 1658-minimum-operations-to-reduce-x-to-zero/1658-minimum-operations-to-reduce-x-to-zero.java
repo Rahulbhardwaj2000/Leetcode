38df787c7b85de0c0e2b4d5c1bd8d1f8206813a3
Rahul Bhardwaj <62097178+Rahulbhardwaj2000@users.noreply.github.com>
2022-06-11 13:58:15 +0530

    Time: 187 ms (10.65%), Space: 188.4 MB (15.97%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        // have to find the largest subarray with that target sum
+        int target = Arrays.stream(nums).sum() - x;
+        // target 0 means the sum of whole array is equal to x
+        if(target == 0) return nums.length;
+        
+        int len = Integer.MIN_VALUE;
+        int presum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1); // this will help to give correct length for array starting from start index
+        
+        for(int i=0;i<nums.length;i++){
+            presum += nums[i];
+            if(map.containsKey(presum - target)){
+                // length of subarray having sum = target
+                //System.out.println(i-map.get(presum - target));
+                len = Math.max(len, i-map.get(presum - target));
+            }
+            map.put(presum, i);
+        }
+        
+        if(len == Integer.MIN_VALUE) return -1;
+        
+        return nums.length - len;
+    }
+    
+}
